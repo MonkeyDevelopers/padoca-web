90e14831de213541d7cc160a7f9b266258d7f83d
Davi Mello <davi.mello@outlook.com.br>
2021-05-14 18:55:00 +0200

    cart_sidebar commit

---
+++ vendor/cart.js
@@ -0,0 +1,40 @@
+function cartSidebar() {
+
+
+    // var sidebar = document.getElementsByClassName("side_cart");
+
+
+}
+
+var button = document.getElementById("cart_button");
+var sidebar = document.getElementById("cart_item");
+
+window.addEventListener('click', function(e){   
+    if (document.getElementById('cart_button').contains(e.target)){
+        if(sidebar.style.right == "-550px") {
+            sidebar.style.right = "0px";
+            button.style.right = "450px";
+    
+        } else {
+            sidebar.style.right = "-550px";
+            button.style.right = "40px";
+        }
+    } else if (document.getElementById('close_card').contains(e.target)) {
+        if(sidebar.style.right == "-550px") {
+            sidebar.style.right = "0px";
+            button.style.right = "450px";
+    
+        } else {
+            sidebar.style.right = "-550px";
+            button.style.right = "40px";
+        }
+    } else if (document.getElementById('cart_item').contains(e.target)) {
+
+    } else {
+        if(sidebar.style.right == "0px") {
+            sidebar.style.right = "-550px";
+            button.style.right = "40px";
+        }
+    }
+
+});
